adc5aaabf755a97af2243c254813b6532e850677
jungin0507 <a01033046758@gmail.com>
2022-11-06 02:18:49 +0900

    refactor: use 'map' to render feature items

--- src/components/ServiceFeatures.tsx
+++ src/components/ServiceFeatures.tsx
@@ -10,7 +10,7 @@ type FeatureItemProps = {
 const FeatureItem = ({ imgSrc, reverse, title, content }: FeatureItemProps) => {
   return (
     <div
-      className={`flex justify-between w-[850px]  h-[373px] ${
+      className={`flex h-[373px] w-[850px]  justify-between ${
         reverse ? 'flex-row-reverse' : 'flex-row'
       }`}
     >
@@ -23,22 +23,31 @@ const FeatureItem = ({ imgSrc, reverse, title, content }: FeatureItemProps) => {
   );
 };
 
+const features: FeatureItemProps[] = [
+  {
+    title: 'Feature 1',
+    content: 'detail of feature',
+    imgSrc: 'https://via.placeholder.com/360x373.png?text=placeholder',
+  },
+  {
+    title: 'Feature 2',
+    content: 'detail of feature',
+    imgSrc: 'https://via.placeholder.com/360x373.png?text=placeholder',
+  },
+];
+
 const ServiceFeatures = () => {
   return (
     <>
       <div className="w-full">
-        <div className="px-7 pb-52 flex flex-col items-center gap-20">
-          <FeatureItem
-            title="Feature 1"
-            content="detail of feature"
-            reverse={true}
-            imgSrc="https://via.placeholder.com/360x373.png?text=placeholder"
-          />
-          <FeatureItem
-            title="Feature 2"
-            content="detail of feature"
-            imgSrc="https://via.placeholder.com/360x373.png?text=placeholder"
-          />
+        <div className="mx-auto flex w-[900px] flex-col gap-20 px-7 pb-52">
+          {features.map((props: FeatureItemProps, idx: number) => (
+            <FeatureItem
+              key={props.title}
+              {...props}
+              reverse={idx % 2 ? true : false}
+            />
+          ))}
         </div>
       </div>
     </>
