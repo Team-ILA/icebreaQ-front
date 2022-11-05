45bedb7e6f0ebd4c4c7a009336d6350c67fd8fd9
jungin0507 <a01033046758@gmail.com>
2022-11-06 00:40:40 +0900

    style: import Montserrat font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Montserrat', ...fontFamily.sans],
+    },
   },
   plugins: [],
 };
