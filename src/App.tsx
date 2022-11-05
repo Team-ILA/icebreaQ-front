ddec77c53eef23c7ff51cec988c1deb8761ed124
jungin0507 <a01033046758@gmail.com>
2022-11-06 00:44:27 +0900

    feat: add landing page route to '/'

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Home from './routes/Home';
+import Landing from './routes/Landing';
 import Makequiz from './routes/MakeQuiz';
 import Quiz from './routes/Quiz';
 
 function App() {
   return (
     <Routes>
-      <Route element={<Home />} index path="/" />
+      <Route element={<Landing />} index path="/" />
+      <Route element={<Home />} index path="/home" />
       <Route element={<Makequiz />} path="/makequiz" />
       <Route element={<Quiz />} path="/quiz/:quizId" />
     </Routes>
