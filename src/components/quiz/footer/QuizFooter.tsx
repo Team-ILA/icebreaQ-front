be6553431c65f0ebbcd60dee802e61544a7e1b82
jungin0507 <a01033046758@gmail.com>
2022-11-07 01:38:57 +0900

    feat: create answer input component

--- src/components/quiz/footer/QuizFooter.tsx
+++ src/components/quiz/footer/QuizFooter.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import MicToggle from './MicToggle';
 import CamToggle from './CamToggle';
+import AnswerInput from './AnswerInput';
 
 const QuizFooter = () => {
   return (
     <div className="fixed bottom-0 flex h-20 w-full items-center gap-4 bg-gray-800 px-10 py-5">
       <MicToggle />
       <CamToggle />
+      <AnswerInput />
     </div>
   );
 };
