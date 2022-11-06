1f727513a5591975df6f3a3a8e5a8eeb03a2e4e6
jungin0507 <a01033046758@gmail.com>
2022-11-06 14:54:22 +0900

    feat: add a method to hide navBar in main layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 import NavBar from './NavBar';
 
 type MainLayoutProps = {
+  hideNavBar?: boolean;
   children: React.ReactNode;
 };
 
-const MainLayout = ({ children }: MainLayoutProps) => {
+const MainLayout = ({ hideNavBar, children }: MainLayoutProps) => {
   return (
     <>
-      <NavBar />
+      {hideNavBar || <NavBar />}
       <main>{children}</main>
     </>
   );
--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { ReactComponent as Logo } from '../logo.svg';
+import MainLayout from '../layouts/MainLayout';
 
 const Login = () => {
-  return <></>;
+  return (
+    <MainLayout hideNavBar>
+      <Logo />
+    </MainLayout>
+  );
 };
 
 export default Login;
