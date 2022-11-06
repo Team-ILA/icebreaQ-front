b49a45c072a182c877f8aaccb7cc7796ecc23008
jungin0507 <a01033046758@gmail.com>
2022-11-06 16:58:01 +0900

    feat: add link to join button

--- src/components/JoinQuizContainer.tsx
+++ src/components/JoinQuizContainer.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DefaultButton from './DefaultButton';
 import { Link } from 'react-router-dom';
 
 const JoinQuizContainer = () => {
+  const [gamePin, setGamePin] = useState('');
+
+  const gamePinChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGamePin(e.target.value);
+  };
+
   return (
     <div className="flex h-full w-[25vw] items-center">
       <div className="flex h-1/4 w-full flex-col justify-center space-y-3 rounded-lg border-2 bg-slate-100 p-6">
@@ -10,11 +16,15 @@ const JoinQuizContainer = () => {
           <p>Game PIN</p>
           <input
             type="text"
+            onChange={gamePinChangeHandler}
+            value={gamePin}
             className="form-control bg-classNameip-padding m-0 block w-full rounded border border-solid border-gray-300 bg-white px-4 py-2 text-xl font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
-            id="gamePIN"
+            id="gamePin"
           />
         </div>
-        <DefaultButton className="w-full p-2" content="Join" />
+        <Link to={`/quiz/${gamePin}`}>
+          <DefaultButton className="w-full p-2" content="Join" />
+        </Link>
         <div className="text-center text-xl">
           <p>or</p>
           <Link className="text-blue-700" to="/makequiz">
