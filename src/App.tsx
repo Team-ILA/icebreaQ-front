bf125136621c9f7da25ba233d3af4d94d369ee5c
jungin0507 <a01033046758@gmail.com>
2022-11-06 16:12:29 +0900

    feat: create join page route to '/join'

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Landing from './routes/Landing';
 import Login from './routes/Login';
 import Makequiz from './routes/MakeQuiz';
 import Quiz from './routes/Quiz';
+import JoinQuiz from './routes/JoinQuiz';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Route element={<Login />} index path="/login" />
       <Route element={<Makequiz />} path="/makequiz" />
       <Route element={<Quiz />} path="/quiz/:quizId" />
+      <Route element={<JoinQuiz />} path="/join" />
     </Routes>
   );
 }
---
+++ src/routes/JoinQuiz.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainLayout from '../layouts/MainLayout';
+
+const JoinQuiz = () => {
+  return (
+    <MainLayout>
+      <></>
+    </MainLayout>
+  );
+};
+
+export default JoinQuiz;
