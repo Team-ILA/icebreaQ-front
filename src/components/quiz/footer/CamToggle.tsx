e9d3afd26fc2028de562418635ab2fd888e1b562
jungin0507 <a01033046758@gmail.com>
2022-11-07 01:38:30 +0900

    feat: create cam toggle button

---
+++ src/components/quiz/footer/CamToggle.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { HiOutlineVideoCamera } from 'react-icons/hi';
+
+const CamToggle = () => {
+  return (
+    <div>
+      <label
+        htmlFor="cam-toggle"
+        className="relative inline-flex cursor-pointer items-center"
+      >
+        <input
+          type="checkbox"
+          value=""
+          id="cam-toggle"
+          className="peer sr-only"
+          defaultChecked={true}
+        />
+        <div className="peer flex h-10 w-10 items-center justify-center rounded-full bg-gray-600 transition-colors after:invisible after:absolute after:h-5 after:w-[2.3px] after:rotate-[-45deg] after:border-[0.5px] after:border-red-600 after:bg-white after:content-[''] peer-checked:bg-red-600 peer-checked:after:visible peer-checked:hover:bg-red-500">
+          <HiOutlineVideoCamera size={17} color="white" />
+        </div>
+      </label>
+    </div>
+  );
+};
+
+export default CamToggle;
--- src/components/quiz/footer/QuizFooter.tsx
+++ src/components/quiz/footer/QuizFooter.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MicToggle from './MicToggle';
+import CamToggle from './CamToggle';
 
 const QuizFooter = () => {
   return (
-    <div className="fixed bottom-0 flex h-20 w-full items-center bg-gray-800 px-10 py-5">
+    <div className="fixed bottom-0 flex h-20 w-full items-center gap-4 bg-gray-800 px-10 py-5">
       <MicToggle />
+      <CamToggle />
     </div>
   );
 };
