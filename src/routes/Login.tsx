e8f71167551c89e178cf011dd4f2e30b9a8ab037
jungin0507 <a01033046758@gmail.com>
2022-11-06 15:45:38 +0900

    feat: create LoginContainer component

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../logo.svg';
 import MainLayout from '../layouts/MainLayout';
+import LoginContainer from '../components/LoginContainer';
 
 const Login = () => {
   return (
     <MainLayout hideNavBar>
-      <Logo />
+      <div className="mx-auto flex h-screen w-1/2 items-center gap-12">
+        <Link className="min-w-fit" to="/">
+          <Logo />
+        </Link>
+        <LoginContainer />
+      </div>
     </MainLayout>
   );
 };
