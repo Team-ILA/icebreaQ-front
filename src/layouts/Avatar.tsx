729a520ca8250cc8729c269febeae9cde9391cc0
jungin0507 <a01033046758@gmail.com>
2022-11-07 19:51:21 +0900

    feat: navbar with avatar

---
+++ src/layouts/Avatar.tsx
@@ -0,0 +1,81 @@
+import React, { Fragment } from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import { HiOutlineUser } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
+import { requestLogOut } from '../lib/api/auth';
+import useAuthAction from '../hooks/useAuthAction';
+
+type AvatarProps = {
+  username: string;
+};
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ');
+}
+
+const Avatar = ({ username }: AvatarProps) => {
+  const { signOut } = useAuthAction();
+  const SignOutClickHandler = () => {
+    signOut();
+    requestLogOut().then(() => {
+      window.location.reload();
+      window.location.href = '/';
+    });
+  };
+
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="ring-gray focus:ring-blue-10 inline-flex h-10 w-10 items-center justify-center rounded-full bg-gray-600 text-sm font-medium shadow-sm hover:bg-gray-500 focus:outline-none focus:ring-[0.5px] focus:ring-offset-2 focus:ring-offset-gray-100">
+          <HiOutlineUser color="white" size={25} />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            <p className="mx-4 border-b-2 py-2 text-right text-sm">
+              hello, <span className="font-medium">{username}</span>
+            </p>
+            <Menu.Item>
+              {({ active }) => (
+                <Link
+                  to="/"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Home
+                </Link>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <button
+                  onClick={SignOutClickHandler}
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block w-full px-4 py-2 text-left text-sm'
+                  )}
+                >
+                  Sign out
+                </button>
+              )}
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
+};
+
+export default Avatar;
--- src/layouts/NavBar.tsx
+++ src/layouts/NavBar.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Avatar from './Avatar';
 import { ReactComponent as NavBarLogo } from '../navbar_logo.svg';
 
+import useAuthValue from '../hooks/useAuthValue';
+
 const NavBar = () => {
+  const auth = useAuthValue();
+
   return (
     <nav className="fixed top-0 z-50 w-full border-b-2 bg-white py-2">
       <div className="mx-auto flex max-w-5xl items-center gap-3">
@@ -11,14 +16,20 @@ const NavBar = () => {
           <NavBarLogo />
         </Link>
         <div className="grow" />
-        <Link to="/login">
-          <button className="font-violet-500 h-9 w-28 px-2 font-bold">
-            Login
-          </button>
-        </Link>
-        <button className="font-violet-500 h-9 w-28 rounded-lg border-2 bg-white px-2 font-bold text-violet-900">
-          Register
-        </button>
+        {auth.username !== '' ? (
+          <Avatar username={auth.username} />
+        ) : (
+          <>
+            <Link to="/login">
+              <button className="font-violet-500 h-9 w-28 px-2 font-bold">
+                Login
+              </button>
+            </Link>
+            <button className="font-violet-500 h-9 w-28 rounded-lg border-2 bg-white px-2 font-bold text-violet-900">
+              Register
+            </button>
+          </>
+        )}
       </div>
     </nav>
   );
