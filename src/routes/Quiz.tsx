b255c812bc0104a0a817b0116fbe49c774f896b4
jungin0507 <a01033046758@gmail.com>
2022-11-08 02:51:37 +0900

    refactor unused code

--- src/routes/Quiz.tsx
+++ src/routes/Quiz.tsx
@@ -1,15 +1,9 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react';
 import MainLayout from '../layouts/MainLayout';
 import QuizContainer from '../components/quiz/QuizContainer';
 import RouteGuard from '../components/RouteGuard';
 
 function Quiz() {
-  const { quizId } = useParams();
-  useEffect(() => {
-    console.log(quizId);
-  }, []);
-
   return (
     <RouteGuard>
       <MainLayout hideNavBar>
