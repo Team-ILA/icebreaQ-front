7450288daffc665d56091eed70ad771e8c9017e5
jungin0507 <a01033046758@gmail.com>
2022-11-07 21:43:41 +0900

    feat: move scroll to bottom

--- src/components/MakeQuizContainer.tsx
+++ src/components/MakeQuizContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useList } from 'react-use';
 import { HiOutlinePlusCircle } from 'react-icons/hi';
 import SelectMenus, { SelectMenuItemProps } from './SelectMenus';
@@ -17,50 +17,58 @@ const initialQuestion: Question = {
 };
 
 const MakeQuizContainer = () => {
+  const scrollRef = useRef<HTMLDivElement>(null);
   const [questions, { push, updateAt, removeAt }] = useList<Question>([
     initialQuestion,
   ]);
 
-  const addQuestionHandler = () => {
-    push(initialQuestion);
+  const addQuestionHandler = async () => {
+    await push(initialQuestion);
+    scrollRef.current?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end',
+      inline: 'end',
+    });
   };
 
   return (
-    <div className="mx-auto my-7 flex w-1/3 flex-col items-center gap-5 rounded-lg bg-gray-50 p-5">
-      <p className="text-2xl font-bold">Making a new quiz</p>
-      <div className="w-full space-y-2 rounded border-2 bg-violet-200 p-3">
-        <div className="mb-3">
-          <p className="font-semibold">Title of Quiz</p>
-          <input
-            type="text"
-            className="form-control bg-classNameip-padding m-0 block w-full rounded border border-solid border-gray-300 bg-white px-3 py-2 text-sm font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
-            id="quiz title"
-          />
+    <div ref={scrollRef} className="pt-[58px]">
+      <form className="mx-auto my-7 flex w-1/3 flex-col items-center gap-5 rounded-lg bg-gray-50 p-5">
+        <p className="text-2xl font-bold">Making a new quiz</p>
+        <div className="w-full space-y-2 rounded border-2 bg-violet-200 p-3">
+          <div className="mb-3">
+            <p className="font-semibold">Title of Quiz</p>
+            <input
+              type="text"
+              className="form-control bg-classNameip-padding m-0 block w-full rounded border border-solid border-gray-300 bg-white px-3 py-2 text-sm font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
+              id="quiz title"
+            />
+          </div>
+          <div>
+            <p>How many people can particiapte in?</p>
+            <SelectMenus items={personnelOptions} />
+          </div>
         </div>
-        <div>
-          <p>How many people can particiapte in?</p>
-          <SelectMenus items={personnelOptions} />
+        {questions.map((question, index) => {
+          return (
+            <QuestionCard
+              question={question}
+              index={index}
+              key={index}
+              updateAt={updateAt}
+              removeAt={removeAt}
+            />
+          );
+        })}
+        <div
+          className="flex w-full items-center justify-center gap-1 border-2 border-dashed bg-white p-3 text-gray-600 hover:cursor-pointer hover:font-bold"
+          onClick={addQuestionHandler}
+        >
+          <HiOutlinePlusCircle size="28" />
+          Add a question
         </div>
-      </div>
-      {questions.map((question, index) => {
-        return (
-          <QuestionCard
-            question={question}
-            index={index}
-            key={index}
-            updateAt={updateAt}
-            removeAt={removeAt}
-          />
-        );
-      })}
-      <div
-        className="flex w-full items-center justify-center gap-1 border-2 border-dashed bg-white p-3 text-gray-600 hover:cursor-pointer hover:font-bold"
-        onClick={addQuestionHandler}
-      >
-        <HiOutlinePlusCircle size="28" />
-        Add a question
-      </div>
-      <DefaultButton className="h-10 w-full" content="Submit" />
+        <DefaultButton className="h-10 w-full" content="Submit" />
+      </form>
     </div>
   );
 };
