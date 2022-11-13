2b9423e9d0169a5abaae524b3deb3a5b7d8bb8d4
jungin0507 <a01033046758@gmail.com>
2022-11-14 01:06:21 +0900

    fix cam & mic issue

--- src/components/quiz/QuizConntection.tsx
+++ src/components/quiz/QuizConntection.tsx
@@ -131,12 +131,37 @@ const QuizConnection = ({ quizId, username }: QuizConnectionProps) => {
   const toggleVideoTrack = () => {
     const myVideo = getMyVideo();
     if (!myVideo) return;
+    if (!camStatus && !audioStatus) {
+      myVideo.stream
+        .getAudioTracks()
+        .forEach((track) => (track.enabled = false));
+      myVideo.stream
+        .getVideoTracks()
+        .forEach((track) => (track.enabled = false));
+      if (myPeer) {
+        const userData: UserDetail = {
+          userId: myPeer.id,
+          quizId: quizId,
+          username: username,
+        };
+        Object.keys(peers).forEach((key) => {
+          myPeer?.call(key, myVideo.stream, {
+            metadata: { id: myPeer.id, user: userData },
+          });
+        });
+      }
+      return;
+    }
     reInitializeStream();
   };
 
   const toggleAudioTrack = () => {
     const myVideo = getMyVideo();
     if (!myVideo) return;
+    if (!camStatus && !audioStatus) {
+      myVideo.stream.getTracks().forEach((track) => track.stop());
+      return;
+    }
     reInitializeStream();
   };
 
