9a51c68cbf4b0ed3e02fd6e8abcafc6cb8f83481
jungin0507 <a01033046758@gmail.com>
2022-11-14 01:32:19 +0900

    feat: display username on cam

--- src/components/quiz/cam/RTCVideo.tsx
+++ src/components/quiz/cam/RTCVideo.tsx
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect, useContext } from 'react';
+import useAuthValue from '../../../hooks/useAuthValue';
 import { VideoItemsContext } from '../../../context/VideoItemsProvider';
 
 type RTCVideoProps = {
@@ -8,6 +9,7 @@ type RTCVideoProps = {
 const RTCVideo = ({ videoId }: RTCVideoProps) => {
   const viewRef = useRef<HTMLVideoElement>(null);
   const [videoItems] = useContext(VideoItemsContext);
+  const auth = useAuthValue();
 
   useEffect(() => {
     if (viewRef.current) {
@@ -16,12 +18,18 @@ const RTCVideo = ({ videoId }: RTCVideoProps) => {
   }, [videoItems]);
 
   return (
-    <video
-      className="h-full w-1/3 bg-black p-2"
-      ref={viewRef}
-      autoPlay
-      muted={videoItems[videoId].userData === undefined}
-    />
+    <div className="h-full w-1/3 bg-black p-2 text-center text-white">
+      <span>
+        {videoItems[videoId].userData === undefined
+          ? auth.username
+          : videoItems[videoId].userData?.username}
+      </span>
+      <video
+        ref={viewRef}
+        autoPlay
+        muted={videoItems[videoId].userData === undefined}
+      />
+    </div>
   );
 };
 
